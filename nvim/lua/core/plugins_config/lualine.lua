7e52bd9fdd51d339d8c44162cfa287f5899c7325
Iago Corrêa <icorrea@icorrea-macos.local>
2024-03-14 00:23:49 -0300

    Lua line fix

--- nvim/lua/core/plugins_config/lualine.lua
+++ nvim/lua/core/plugins_config/lualine.lua
@@ -1,32 +1,14 @@
-local lualine = require('lualine')
-
-
 require('lualine').setup {
   options = {
-    theme = 'gruvbox_dark',
-    component_separators = '',
-    section_separators = { left = '', right = '' },
+    icons_enabled = true,
+    theme = 'gruvbox-material',
   },
   sections = {
-    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = {
-      '%=', --[[ add your center compoentnts here in place of this comment ]]
-    },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
+    lualine_a = {
+      {
+        'filename',
+        path = 1,
+      }
+    }
+  }
 }
