2806b2a9e19c9ee09fe0c92f4617731fd79572fe
Iago Corrêa <icorrea@icorrea-macos.local>
2024-05-19 23:42:50 -0300

    New keymaps.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,19 +1,29 @@
-local function opts(desc)
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+local function opts_nvimtree(desc)
     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 end
 
 vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
 
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+keymap.set('n', '<space>e', vim.diagnostic.open_float)
+keymap.set('n', '[d', vim.diagnostic.goto_prev)
+keymap.set('n', ']d', vim.diagnostic.goto_next)
+keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-vim.keymap.set('n', '<C-t>', ':NvimTreeToggle<CR>', opts('Toggle'))
-vim.keymap.set('n', '<leader>n',':NvimTreeFocus<CR>', opts('Focus'))
-vim.keymap.set('n', '<C-n>',':NvimTreeOpen<CR>', opts('Open'))
-vim.keymap.set('n', '<C-f>',':NvimTreeFindFile<CR>', opts('Find'))
+keymap.set('n', '<C-t>', ':NvimTreeToggle<CR>', opts_nvimtree('Toggle'))
+keymap.set('n', '<leader>n',':NvimTreeFocus<CR>', opts_nvimtree('Focus'))
+keymap.set('n', '<C-n>',':NvimTreeOpen<CR>', opts_nvimtree('Open'))
+keymap.set('n', '<C-f>',':NvimTreeFindFile<CR>', opts_nvimtree('Find'))
 
-vim.keymap.set('n', '<F1>', '<Cmd>BufferPrevious<CR>', opts('BufferPrevious'))
-vim.keymap.set('n', '<F2>', '<Cmd>BufferNext<CR>', opts('BufferNext'))
-vim.keymap.set('n', '<F3>', '<Cmd>BufferClose<CR>', opts('BufferClose'))
+keymap.set('n', '<s-tab>', '<Cmd>BufferPrevious<CR>', opts)
+keymap.set('n', '<tab>', '<Cmd>BufferNext<CR>', opts)
+keymap.set('n', 'q<tab>', '<Cmd>BufferClose<CR>', opts)
+
+keymap.set('n', 'ss', ':split<Return>', opts)
+keymap.set('n', 'sv', ':vsplit<Return>', opts)
+keymap.set('n', 'sw', '<C-w>w', opts)
+
+keymap.set('n', '<leader>l', ':Lazy<Return>', opts)
+keymap.set('n', '<leader>m', ':Mason<Return>', optq)
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,31 +1,31 @@
 require("lazy").setup({
 
-	-- Dependencies
-	{ "nvim-tree/nvim-web-devicons" },
-	{ "nvim-lua/plenary.nvim" },
+    -- Dependencies
+    { "nvim-tree/nvim-web-devicons" },
+    { "nvim-lua/plenary.nvim" },
     { "MunifTanjim/nui.nvim" },
-	
-	-- Plugins
+    
+    -- Plugins
     { "phha/zenburn.nvim" },
-	{ "nvim-tree/nvim-tree.lua" },
-	{ "nvim-lualine/lualine.nvim" },
-	{ "lewis6991/gitsigns.nvim" },
-	{ "windwp/nvim-ts-autotag" },
-	{ "windwp/nvim-autopairs" },
-	{ "romgrk/barbar.nvim" },
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.6" },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-tree/nvim-tree.lua" },
+    { "nvim-lualine/lualine.nvim" },
+    { "lewis6991/gitsigns.nvim" },
+    { "windwp/nvim-ts-autotag" },
+    { "windwp/nvim-autopairs" },
+    { "romgrk/barbar.nvim" },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.6" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "VonHeikemen/fine-cmdline.nvim" },
 
-	-- Completion
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "L3MON4D3/LuaSnip" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{ "rafamadriz/friendly-snippets" },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "neovim/nvim-lspconfig" }
+    -- Completion
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "L3MON4D3/LuaSnip" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "rafamadriz/friendly-snippets" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" }
 
 })
