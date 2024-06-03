5ff57804ecab232098c4237731eef2992c143e64
Iago Corrêa <icorrea@icorrea-macos.local>
2024-06-03 20:28:23 -0300

    Fix completions.

--- nvim/lua/core/plugins_config/completions.lua
+++ nvim/lua/core/plugins_config/completions.lua
@@ -8,14 +8,10 @@ cmp.setup({
     
     formatting = {
         format = lspkind.cmp_format({
-            mode = "symbol",
+            mode = "symbol_text",
             maxwidth = 50,
             ellipsis_char = "...",
-            show_labelDetails = true,
-
-            before = function (entry, vim_item)
-                return vim_item
-            end
+            show_labelDetails = true
         }) 
     },
 
